e6c0bbc64fd652f9a9693b5dacf46bb1b17a0c3c
Faisal Amir <urmauur@gmail.com>
2024-02-14 02:33:23 +0700

    preapre expandable filter hub screen

--- web/screens/Hub/index.tsx
+++ web/screens/Hub/index.tsx
@@ -1,7 +1,11 @@
+import { useState } from 'react'
+
 import { Input, Button, useDebouncedState } from '@janhq/joi'
 
 import { useAtomValue } from 'jotai'
 import { SearchIcon, Settings2Icon } from 'lucide-react'
+
+import { twMerge } from 'tailwind-merge'
 
 import styles from './hub.module.scss'
 
@@ -14,6 +18,7 @@ export default function HubScreen() {
   const [searchValue, setsearchValue] = useDebouncedState('', 400)
   const configuredModels = useAtomValue(configuredModelsAtom)
   const downloadedModels = useAtomValue(downloadedModelsAtom)
+  const [showFilter, setShowFilter] = useState(false)
 
   // const filteredModels = configuredModels.filter((x) => {
   //   if (sortSelected === 'Downloaded') {
@@ -37,34 +42,48 @@ export default function HubScreen() {
 
   return (
     <div className={styles.hub}>
-      <div className={styles.hubHeader}>
-        <div className="mx-auto flex h-full w-2/5 flex-col items-center justify-center">
-          <h1 className="mb-4 text-2xl font-bold">Discover models</h1>
-          <div className="w-full">
-            <div className="flex w-full gap-x-2">
-              <Input
-                prefixIcon={<SearchIcon size={16} />}
-                placeholder="Search models"
-                onChange={(e) => {
-                  setsearchValue(e.target.value)
-                }}
-              />
-              <Button theme="secondary">
-                <Settings2Icon size={16} />
-              </Button>
-            </div>
-            <div className="mt-4">
-              <Button size="small" variant="soft" asChild>
-                <a
-                  href="https://jan.ai/guides/using-models/import-manually/"
-                  target="_blank"
+      <div className={styles.hubContent}>
+        <div className={styles.hubHeader}>
+          <div className="mx-auto flex h-full flex-col items-center justify-center px-4 lg:w-1/2">
+            <h1 className="mb-4 text-2xl font-bold">Discover models</h1>
+            <div className="w-full">
+              <div className="flex w-full gap-x-2">
+                <Input
+                  prefixIcon={<SearchIcon size={16} />}
+                  placeholder="Search models"
+                  onChange={(e) => {
+                    setsearchValue(e.target.value)
+                  }}
+                />
+                <Button
+                  theme="secondary"
+                  onClick={() => setShowFilter(!showFilter)}
                 >
-                  How to manually import models
-                </a>
-              </Button>
+                  <Settings2Icon size={16} />
+                </Button>
+              </div>
+              <div className="mt-4">
+                <Button size="small" variant="soft" asChild>
+                  <a
+                    href="https://jan.ai/guides/using-models/import-manually/"
+                    target="_blank"
+                  >
+                    How to manually import models
+                  </a>
+                </Button>
+              </div>
             </div>
           </div>
         </div>
+      </div>
+
+      <div className={twMerge(styles.hubFilter, showFilter && styles.isShow)}>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
+          voluptatem culpa consectetur eos ab, hic eius. Iste corporis libero
+          hic similique provident, possimus tempore iure, cumque modi inventore
+          ducimus tenetur.
+        </p>
       </div>
     </div>
   )
